6f76a9e7f1631ae5bea82fbb1c6a6744215a90e1
clemente0731 <32806348+clemente0731@users.noreply.github.com>
2024-04-01 13:40:53 +0000

    refine build-sys

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -exuo pipefail
 
+# 检查当前用户是否为 root
+if [ "$(id -u)" -ne 0 ]; then
+    SUDO_CMD="sudo"
+fi
+
+$SUDO_CMD apt update || true
+$SUDO_CMD apt install libcapstone-dev ninja-build || true
 
 project_dir=$(git rev-parse --show-toplevel)
 cd ${project_dir}
